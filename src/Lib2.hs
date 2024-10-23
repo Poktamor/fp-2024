8755d1cb3263dd402bab0c787b4cdb8c7a495a30
Poktamor <ruslanui.m@gmail.com>
2024-10-24 01:55:33 +0300

    updated repo and added new grammar in bnf file

---
+++ src/Lib2.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE InstanceSigs #-}
+module Lib2
+    ( Query(..),
+    parseQuery,
+    State(..),
+    emptyState,
+    stateTransition
+    ) where
+
+-- | An entity which represets user input.
+-- It should match the grammar from Laboratory work #1.
+-- Currently it has no constructors but you can introduce
+-- as many as needed.
+data Query
+  = CreateSubmarine SubmarineType String Int
+  | Fillballast Int Int Int
+  | Fillballast Int Int Int
+  | ManipulateAirlock OpenState
+  | Sequence [Query]
+  deriving (Eq, Show)
+
+data SubmarineType = Attack | Scout | DeepDive
+ deriving (Eq, Show)
+
+data FillStatus = Liters Int | MiliLiters Int
+ deriving (Eq, Show)
+
+data 
+
+
+-- | The instances are needed basically for tests
+instance Eq Query where
+  (==) _ _= False
+
+instance Show Query where
+  show _ = ""
+
+-- | Parses user's input.
+-- The function must have tests.
+parseQuery :: String -> Either String Query
+parseQuery _ = Left "Not implemented 2"
+
+-- | An entity which represents your program's state.
+-- Currently it has no constructors but you can introduce
+-- as many as needed.
+data State
+
+-- | Creates an initial program's state.
+-- It is called once when the program starts.
+emptyState :: State
+emptyState = error "Not implemented 1"
+
+-- | Updates a state according to a query.
+-- This allows your program to share the state
+-- between repl iterations.
+-- Right contains an optional message to print and
+-- an updated program's state.
+stateTransition :: State -> Query -> Either String (Maybe String, State)
+stateTransition _ _ = Left "Not implemented 3"
